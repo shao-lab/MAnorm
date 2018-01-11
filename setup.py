35a377d6c3e00f9ec9d92ddc8b138a519a1458cf
haydensun <hayden_sun@hotmail.com>
2018-01-11 16:55:04 +0800

    Update README

--- setup.py
+++ setup.py
@@ -4,34 +4,8 @@ from setuptools import setup, find_packages
 from manorm import __version__
 
 DESCRIPTION = "A robust model for quantitative comparison of ChIP-Seq data sets."
-LONG_DESCRIPTION = """ChIP-Seq is widely used to characterize genome-wide binding patterns of transcription factors 
-and other chromatin-associated proteins. Although comparison of ChIP-Seq data sets is critical for understanding the 
-role of their cell type-specific binding on modulating gene regulation programs, few quantitative approaches have 
-been developed. Here, we present a simple and effective method, MAnorm, for quantitative comparison of ChIP-Seq data 
-sets describing transcription factor binding sites and epigenetic modifications. The quantitative binding differences 
-inferred by MAnorm showed strong correlation with both the changes in expression of target genes and the binding of 
-cell type-specific regulators. 
-
-MAnorm uses common peaks of two samples as a reference to build the rescaling model for normalization, which is based 
-on the empirical assumption that if a chromatin-associated protein has a substantial number of peaks shared in two 
-conditions, the binding at these common regions will tend to be determined by similar mechanisms, and thus should  
-exhibit similar global binding intensities across samples. 
-
-The normalized M value given by MAnorm was used as a quantitative measure of differential binding in each peak region 
-between two samples, with peak regions associated with larger absolute M values exhibiting greater binding differences 
-between two samples.  
-
-MAnorm exhibited excellent performance in quantitative comparison of ChIP-Seq data sets for both epigenetic 
-modifications and transcription factors. The quantitative binding differences inferred by MAnorm were highly 
-correlated with both the changes in expression of target genes and also the binding of cell type-specific regulators. 
-With the accumulation of ChIP-seq data sets, MAnorm should serve as a powerful tool for obtaining a more 
-comprehensive understanding of cell type-specific and cell state-specific regulation during organism development and 
-disease onset. 
-
-**Citation** MAnorm: a robust model for quantitative comparison of ChIP-Seq data sets. Shao Z, Zhang Y, Yuan GC, 
-Orkin SH, Waxman DJ. Genome Biol. Mar 16;13(3):R16. 
-
-"""
+with open('README.rst') as fin:
+    LONG_DESCRIPTION = fin.read()
 
 INSTALL_REQUIRES = ["numpy",
                     "matplotlib",
