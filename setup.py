dbf6b46dba9754b7db5fabeffa42c5b9124addef
haydensun <hayden_sun@hotmail.com>
2018-11-21 17:22:24 +0800

    Update setup.py for v1.2.0

--- setup.py
+++ setup.py
@@ -1,41 +1,66 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-from manorm import __version__
+import io
+import sys
 
-DESCRIPTION = "A robust model for quantitative comparison of ChIP-Seq data sets."
-with open('README.rst') as fin:
-    LONG_DESCRIPTION = fin.read()
+from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = ["numpy",
-                    "matplotlib",
-                    "statsmodels",
-                    "scipy"]
+import manorm
 
-CLASSIFIERS = ["Development Status :: 5 - Production/Stable",
-               "Environment :: Console",
-               "Intended Audience :: Science/Research",
-               "License :: OSI Approved :: BSD License",
-               "Operating System :: OS Independent",
-               "Programming Language :: Python",
-               "Programming Language :: Python :: 2",
-               "Programming Language :: Python :: 2.7",
-               "Topic :: Scientific/Engineering :: Bio-Informatics"]
+py_version = sys.version_info[:2]
+if not (py_version == (2, 7) or py_version >= (3, 4)):
+    raise RuntimeError("MAnorm requires Python 2.7 or 3.4+ to install!")
+
+description = "A robust model for quantitative comparison of ChIP-Seq data sets."
+with io.open('README.rst', encoding='utf-8') as fin:
+    long_description = fin.read()
+
+install_requires = [
+    "numpy",
+    "matplotlib",
+    "statsmodels",
+    "scipy"
+]
+
+extras_require = {
+    "test": ["pytest >= 3.0.0",
+             "pytest-cov"],
+    "docs": ["sphinx >= 1.6.3",
+             "sphinx_rtd_theme"]
+}
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: Scientific/Engineering :: Bio-Informatics"
+]
 
 setup(
     name="MAnorm",
-    version=__version__,
-    packages=find_packages(exclude=["test"]),
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    version=manorm.__version__,
+    description=description,
+    long_description=long_description,
+    author="Hayden Sun",
+    author_email="sunhongduo@picb.ac.cn",
     url="https://github.com/shao-lab/MAnorm",
-    author="Semal",
-    author_email="",
-    maintainer="Hayden Sun",
-    maintainer_email="sunhongduo@picb.ac.cn",
     license="BSD",
-    entry_points={"console_scripts": ["manorm=manorm.cmdline:main"]},
-    install_requires=INSTALL_REQUIRES,
-    classifiers=CLASSIFIERS,
+    packages=find_packages(),
+    entry_points={"console_scripts": ["manorm=manorm.cli:main"]},
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    install_requires=install_requires,
+    extras_require=extras_require,
+    classifiers=classifiers,
     zip_safe=False,
 )
