fbccb19d042284fa96454eef87d956ac97737990
haydensun <hayden_sun@hotmail.com>
2018-12-06 13:47:30 +0800

    Improve cli behavior

--- manorm/cli.py
+++ manorm/cli.py
@@ -23,15 +23,14 @@ def argparser_config():
     description = """MAnorm -- A robust model for quantitative comparison of ChIP-seq data sets."""
     epilog = dedent("""
     Citation:
-    
-    Shao Z, Zhang Y, Yuan GC, Orkin SH, Waxman DJ. MAnorm: a robust model for quantitative 
-    comparison of ChIP-Seq data sets. Genome biology. 2012 Mar 16;13(3):R16.
-    https://doi.org/10.1186/gb-2012-13-3-r16
+      Shao Z, Zhang Y, Yuan GC, Orkin SH, Waxman DJ. MAnorm: a robust model for quantitative 
+      comparison of ChIP-Seq data sets. Genome biology. 2012 Mar 16;13(3):R16.
+      https://doi.org/10.1186/gb-2012-13-3-r16
     
     For more information, please visit:
-    Documentation: https://manorm.readthedocs.io
-    Source code: https://github.com/shao-lab/MAnorm
-    Report bugs: https://github.com/shao-lab/MAnorm/issues
+      Documentation: https://manorm.readthedocs.io
+      Source code: https://github.com/shao-lab/MAnorm
+      Bug reports: https://github.com/shao-lab/MAnorm/issues
     """)
 
     parser = argparse.ArgumentParser(description=description, epilog=epilog,
@@ -54,10 +53,10 @@ def argparser_config():
                             choices=READ_FORMATS, default="bed",
                             help="Format of read files. Supported: {}. Default: bed".format(READ_FORMATS))
     input_args.add_argument("--n1", "--name1", metavar="NAME", dest="name1", default=None,
-                            help="Name of sample 1 (i.e. experiment condition/cell-type), which is used in output "
-                                 "files. If not specified, peak/read file names will be used as sample names.")
+                            help="Name of sample 1, which is used in output files. If not specified, the name of the "
+                                 "peak file will be used as the sample name.")
     input_args.add_argument("--n2", "--name2", metavar="NAME", dest="name2", default=None,
-                            help="Name of sample 2.")
+                            help="Name of sample 2, same as `--n1/--name1`.")
 
     reads_args = parser.add_argument_group("Reads Manipulation")
     reads_args.add_argument("--s1", "--shiftsize1", metavar="N", dest="shift_size1", type=int, default=100,
@@ -66,10 +65,10 @@ def argparser_config():
                                  "underlying DNA fragment. Set to half of DNA fragment size of the ChIP-seq library. "
                                  "Default: 100")
     reads_args.add_argument("--s2", "--shiftsize2", metavar="N", dest="shift_size2", type=int, default=100,
-                            help="Reads shift size of sample 2. Default: 100")
-    reads_args.add_argument("--paired", dest="paired", action='store_true', default=False,
+                            help="Reads shift size of sample 2, same as `--s1/--shiftsize1`. Default: 100")
+    reads_args.add_argument("--pe", "--paired-end", dest="paired", action='store_true', default=False,
                             help="Paired-end mode. The middle point of each read pair is used to represent the genomic "
-                                 "locus of underlying DNA fragment. `--s1`, `--s2` are ignored with this option on.")
+                                 "locus of underlying DNA fragment. `--s1` and `--s2` are ignored with this option on.")
 
     model_args = parser.add_argument_group("Normalization Model")
     model_args.add_argument("-w", "--window-size", metavar="LENGTH", dest="window_size", type=pos_int, default=2000,
@@ -77,7 +76,7 @@ def argparser_config():
                                  "sharp histone marks like H3K4me3 and H3K27ac, and 1000 for TFs or DNase-seq. "
                                  "Default: 2000")
     model_args.add_argument("--summit-dis", metavar="LENGTH", dest="summit_dis", type=pos_int, default=None,
-                            help="Overlapping common peaks with summit-to-summit distance above this are excluded "
+                            help="Overlapping common peaks with summit-to-summit distance beyond this are excluded "
                                  "in model fitting. Default: `-w/--window-size` / 4")
     model_args.add_argument("--n-random", metavar="N", dest="n_random", type=pos_int, default=10,
                             help="Number of random simulations to test the enrichment of peak overlap between two "
@@ -89,12 +88,12 @@ def argparser_config():
                                   "Default: 1.0")
     output_args.add_argument("-p", "--p-cutoff", metavar="FLOAT", dest="p_cutoff", type=float, default=0.01,
                              help="P-value cutoff to define biased peaks. Default: 0.01")
-    output_args.add_argument("--write-all", dest="write_all", action="store_true", default=False,
+    output_args.add_argument("--oa", "--output-all", dest="output_all", action="store_true", default=False,
                              help="Write two extra output files containing the results of original (unmerged) peaks.")
+    output_args.add_argument("--dir", "--output-dir", metavar="DIR", dest="output_dir", default=None,
+                             help="Output directory. Default: Current working directory")
     output_args.add_argument("--verbose", dest="verbose", action="store_true", default=False,
                              help="Enable verbose log messages.")
-    output_args.add_argument("--dir", "--output-dir", metavar="DIR", dest="output_dir", default=None,
-                             help="Output directory path. Default: Current working directory")
     return parser
 
 
@@ -118,11 +117,11 @@ def pos_int(value):
 
 def main():
     """MAnorm main entry point.
-    This function parses arguments and passes them to an instance of :class:`MAnorm` to run MAnorm application.
+    This function parses arguments and passes them to the MAnorm application.
     """
     parser = argparser_config()
     args = parser.parse_args()
-    app = application.MAnorm(args)
+    app = application.MAnorm(**vars(args))
     app.run()
 
 
