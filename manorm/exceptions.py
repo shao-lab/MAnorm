a51755c5abd1afc302d8724cd1be198369276c4e
haydensun <hayden_sun@hotmail.com>
2018-12-11 16:09:06 +0800

    Better format string for PeakFormatError

--- manorm/exceptions.py
+++ manorm/exceptions.py
@@ -11,12 +11,17 @@ This module contains the exceptions of MAnorm.
 class PeakFormatError(Exception):
     """Invalid peak format error."""
 
-    def __init__(self, format, line):
+    def __init__(self, format, line, line_num=None):
         self.format = format
         self.line = line.rstrip()
+        self.line_num = line_num
 
     def __str__(self):
-        return "Invalid {} format: {!r}".format(self.format, self.line)
+        if self.line_num is None:
+            msg = "Invalid {} format: {!r}".format(self.format, self.line)
+        else:
+            msg = "Invalid {} format at line {}: {!r}".format(self.format, self.line_num, self.line)
+        return msg
 
 
 class UnknownFormatError(Exception):
