c26160264ce5da3d772c59b196645a96b94b8ef0
haydensun <hayden_sun@hotmail.com>
2018-12-19 16:23:54 +0800

    Update __init__.py

--- manorm/__init__.py
+++ manorm/__init__.py
@@ -1,1 +1,15 @@
-__version__ = "1.2.0"
+# -*- coding: utf-8 -*-
+
+"""
+manorm
+~~~~~~
+
+A robust model for quantitative comparison of ChIP-seq data sets.
+"""
+
+from __future__ import absolute_import
+
+from manorm.application import MAnorm
+from manorm.logging import logger
+
+__version__ = '1.2.0'
