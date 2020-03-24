1ac88425f44127cbc61b6906c00c7863f2ec87d9
haydensun <hayden_sun@hotmail.com>
2020-03-24 13:57:21 +0800

    Update exceptions

--- manorm/exceptions.py
+++ manorm/exceptions.py
@@ -1,41 +1,28 @@
-# -*- coding: utf-8 -*-
-
 """
 manorm.exceptions
-~~~~~~~~~~~~~~~~~
+-----------------
 
 This module contains the exceptions of MAnorm.
 """
 
 
-class InvalidFormatError(Exception):
-    """Invalid file format error."""
-
-    def __init__(self, format, line, line_num=None):
-        self.format = format
-        self.line = line.rstrip()
-        self.line_num = line_num
-
-    def __str__(self):
-        if self.line_num is None:
-            msg = "Invalid {} format: {!r}".format(self.format, self.line)
-        else:
-            msg = "Invalid {} format at line {}: {!r}".format(self.format, self.line_num, self.line)
-        return msg
+class ManormError(Exception):
+    """Base class for MAnorm errors."""
 
 
-class UnsupportedFormatError(Exception):
-    """Unsupported format error."""
-
-    def __init__(self, format):
-        self.format = format
-
-    def __str__(self):
-        return "Unsupported format: {!r}".format(self.format)
+class FileFormatError(ManormError):
+    def __init__(self, format, line_num, line):
+        msg = f"Invalid {format} format at line {line_num}: {line!r}"
+        super().__init__(msg)
 
 
-class UnmatchedBedFormatError(Exception):
-    """Unmatched format error."""
+class FormatModeConflictError(Exception):
+    def __init__(self, format, mode):
+        msg = f"format {format!r} is conflict with mode {mode!r}"
+        super().__init__(msg)
 
-    def __str__(self):
-        return "Unmatched format: please use BED format for single-end reads and BEDPE for paired-end reads"
+
+class ProcessNotReadyError(ManormError):
+    def __init__(self, step, precursor):
+        msg = f"Unable to {step}, please {precursor} first"
+        super().__init__(msg)
